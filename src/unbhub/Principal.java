20b5630a76ab747b9b02bbf10f146d3b04fe229e
Daniel <Daniel@DESKTOP-SCBAE9E>
2023-01-13 23:03:47 -0300

    Pacotes e classes para controle, criei arquivo css, organizei melhor, classe principal executável

--- src/unbhub/Principal.java
+++ src/unbhub/Principal.java
@@ -5,11 +5,9 @@
 package unbhub;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -19,21 +17,13 @@ import javafx.stage.Stage;
 public class Principal extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage primaryStage) throws Exception {
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Parent root = FXMLLoader.load(getClass().getResource("TelaCadastro.fxml"));
+        Scene scene = new Scene(root);
         
-        Scene scene = new Scene(root, 300, 250);
+        String css = this.getClass().getResource("estilo.css").toExternalForm();
+        scene.getStylesheets().add(css);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
