a1df75b10d2f801d2b5245eb364c7699cd034b04
Daniel <Daniel@DESKTOP-SCBAE9E>
2023-01-14 16:55:13 -0300

    Fontes customizadas, css configurado, modificações gerais na tela inicial

--- src/unbhub/Principal.java
+++ src/unbhub/Principal.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
- */
 package unbhub;
 
 import javafx.application.Application;
@@ -10,29 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Daniel
- */
+
 public class Principal extends Application {
     
     @Override
     public void start(Stage primaryStage) throws Exception {
         
-        Parent root = FXMLLoader.load(getClass().getResource("TelaCadastro.fxml"));
-        Scene scene = new Scene(root);
+        Parent root = FXMLLoader.load(getClass().getResource("/telas/TelaCadastro.fxml"));
+        Scene scene = new Scene(root,640,360);
         
-        String css = this.getClass().getResource("estilo.css").toExternalForm();
+        String css = this.getClass().getResource("/telas/estilo.css").toExternalForm();
         scene.getStylesheets().add(css);
         
         primaryStage.setTitle("Hello World!");
+        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
