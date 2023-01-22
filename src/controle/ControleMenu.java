23701d085a3dc3bacaf13cf746940fe02d203c3c
Bernardo Rabello <53843870+BernardoRabello@users.noreply.github.com>
2023-01-22 11:17:35 -0300

    Implementei cadastro/login

--- src/controle/ControleMenu.java
+++ src/controle/ControleMenu.java
@@ -11,7 +11,14 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import unbhub.Principal;
+import unbhub.Cliente;
+import unbhub.Usuario;
+
 
 public class ControleMenu implements Initializable {
 
@@ -20,6 +27,11 @@ public class ControleMenu implements Initializable {
     private Parent root;
     @FXML
     private Button btnSair,btnCadastro;
+    @FXML
+    private TextField txtNome, txtUsername, txtSenha, txtCpf;
+    @FXML
+    private Label lblErro;
+
     double x=0,y=0;
     
     // Troca a tela de Cadastro pela tela de Login
@@ -67,39 +79,86 @@ public class ControleMenu implements Initializable {
         });
     }
     
-    // ** CRIAR OBJETO CLIENTE E FAZER VERIFICAÇÃO SE JÁ EXISTE ETC **
+    
     // Criar um novo usuário
     public int criarCliente(){
-        // ** RETORNAR ERROS E DAR UM NÚMERO, SEM ERRO É 0 E CRIAR O CLIENTE **
+        String nome = txtNome.getText(), 
+               senha = txtSenha.getText(),
+               username = txtUsername.getText(),
+               cpf = txtCpf.getText();
+        
+        // Checa se todos os campos estão preenchidos
+        if (nome.equals("") || senha.equals("") || username.equals("") || cpf.equals("")) {
+            return 2;
+        }
+        
+        // Checa se nome de usuario ja existe
+        for (Usuario user : Principal.usuarios) {
+            if (user.getUsername().equals(username)) {
+                return 1;
+            }    
+        }
+        
+        Principal.usuarios.add(new Cliente(txtNome.getText(), txtSenha.getText(), txtUsername.getText(), Integer.parseInt(txtCpf.getText()), Principal.cIds));
+        Principal.cIds += 1;
         return 0;
     }
     
     
     
-    // Troca para a tela de entrada
+    // Realiza cadastro
     public void entrarCadastro(ActionEvent event) throws IOException {
         switch (criarCliente()) {
             case 2:
-                //Erro...
+                //Caso algum campo nao esteja preenchido:
+                lblErro.setTextFill(Color.color(1,0,0));
+                lblErro.setText("Preencha todos os campos");
                 break;
             case 1:
-                //Erro...
+                //caso username ja exista:
+                lblErro.setTextFill(Color.color(1,0,0));
+                lblErro.setText("Nome de Usuário ja existe");
                 break;
             case 0:
-                root = FXMLLoader.load(getClass().getResource("/telas/TelaEntrada.fxml"));
-                stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-                scene = new Scene(root);
-
-                // Aplicar estilo css na cena
-                String css = this.getClass().getResource("/telas/estilo.css").toExternalForm();
-                scene.getStylesheets().add(css);
-
-                telaArrastavel(root,stage);
-
-                stage.setScene(scene);
-                stage.show();
+                lblErro.setTextFill(Color.color(0,1,0));
+                lblErro.setText("Usuário cadastrado com sucesso");
+                txtNome.setText("");
+                txtSenha.setText("");
+                txtCpf.setText("");
+                txtUsername.setText("");
                 break;
         }
+    }
+    
+    
+    // Confere nome e senha e então realiza o login
+    public void realizarLogin(ActionEvent event) throws IOException {
+        String user = txtUsername.getText(), senha = txtSenha.getText();
+        for (Usuario u : Principal.usuarios) {
+            if (u.getUsername().equals(user)) {
+                if (u.checkSenha(senha)) {
+                    root = FXMLLoader.load(getClass().getResource("/telas/TelaEntrada.fxml"));
+                    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                    scene = new Scene(root);
+
+                    // Aplicar estilo css na cena
+                    String css = this.getClass().getResource("/telas/estilo.css").toExternalForm();
+                    scene.getStylesheets().add(css);
+
+                    telaArrastavel(root,stage);
+
+                    stage.setScene(scene);
+                    stage.show();
+                    return;
+                } else {
+                    // Senha incorreta:
+                    lblErro.setText("Senha incorreta");
+                }
+            }
+        }
+        // Usuario não existe:
+        lblErro.setText("Usuário não encontrado");
+        
     }
     
     
